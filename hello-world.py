c398b6e025d81be15da5b74ab821498086c1ad8e
pehara <130656028+pehara@users.noreply.github.com>
2024-10-15 17:27:05 -0700

    Added a Hello World Program
    This repo is less boring now.

---
+++ hello-world.py
@@ -0,0 +1,13 @@
+# This program will write "Hello World!"
+
+# Hello world function
+def PrintHelloWorld():
+    print("Hello World!")
+  
+# Main compiler
+def main():
+    PrintHelloWorld()
+ 
+# Failsafe statement
+if __name__ =="main":
+    main()  
